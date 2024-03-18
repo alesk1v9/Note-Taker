5774c2890806eb68aa4d6bbd1bb8b77d240b254e
alesk1v9 <alexsanderrafael2022@gmail.com>
2024-03-18 19:32:56 -0400

    fixed bug on notes html path

--- routes/htmlRouter.js
+++ routes/htmlRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const htmlRouter = express.Router();
+const path = require('path');
 
 htmlRouter.get('/', (req, res) => {
-    res.sendFile('C:/Users/alexs/OneDrive/Documentos/Coding Bootcamp/challenges/challenge 9 - Note Taker!/public/index.html');
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 htmlRouter.get('/notes', (req, res) => {
-    res.sendFile('C:/Users/alexs/OneDrive/Documentos/Coding Bootcamp/challenges/challenge 9 - Note Taker!/public/notes.html'); 
+    res.sendFile(path.join(__dirname, '../public/notes.html')); 
 });
 
 module.exports = htmlRouter;
